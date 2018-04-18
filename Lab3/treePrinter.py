b9ef3c655641212ec05321b6a053989341ccbcdf
Mateusz <mateusz14321@gmail.com>
2018-04-18 19:53:10 +0200

    Surround with str(), diagnostic for classes

--- Lab3/treePrinter.py
+++ Lab3/treePrinter.py
@@ -21,7 +21,7 @@ class TreePrinter:
         res += self.instructions.printTree(indent)
         return res
 
-    @addToClass(classes.BinExpr)
+    @addToClass(classes.BinExpr)    #!!! doesn't work Treeprinting for INT type !!!
     def printTree(self, indent=0):
         res = indent * indent_char + str(self.op) + '\n'
         res += self.left.printTree(indent + 1)
@@ -30,38 +30,43 @@ class TreePrinter:
 
     @addToClass(classes.UnExpr)
     def printTree(self, indent=0):
-        res = indent * indent_char + str(self.operator) + '\n'
+        res = ""
+        res += indent * indent_char + str(self.operator) + '\n'
         res += self.expressions.printTree(indent + 1)
+        return res
 
-    @addToClass(classes.Variable)
+    @addToClass(classes.Variable)  # works fine
     def printTree(self, indent=0):
         res = indent * indent_char + str(self.value) + '\n'
         return res
 
-    @addToClass(classes.MatrixReference)
+    @addToClass(classes.MatrixReference)  # works fine
     def printTree(self, indent=0):
         res = indent * indent_char + 'REF\n'
         res += (indent + 1) * indent_char + str(self.matrix_id) + '\n'
         res += self.locations.printTree(indent + 1)
         return res
 
-    @addToClass(classes.MatrixLocations)
+    @addToClass(classes.MatrixLocations)  # works fine
     def printTree(self, indent=0):
-        res = indent * indent_char + self.dim_locations.printTree(indent) if issubclass(self.dim_locations.__class__, classes.Node) else indent * indent_char + str(self.dim_locations) + '\n'
-        res += indent * indent_char + self.location.printTree(indent) if issubclass(self.dim_locations.__class__, classes.Node) else indent * indent_char + str(self.location) + '\n'
+        res = indent * indent_char + self.dim_locations.printTree(indent) if issubclass(self.dim_locations.__class__,
+                                                                                        classes.Node) else indent * indent_char + str(
+            self.dim_locations) + '\n'
+        res += indent * indent_char + self.location.printTree(indent) if issubclass(self.dim_locations.__class__,
+                                                                                    classes.Node) else indent * indent_char + str(
+            self.location) + '\n'
         return res
 
-
-    @addToClass(classes.EndAssignment)
+    @addToClass(classes.EndAssignment)  # works fine
     def printTree(self, indent=0):
         res = indent * indent_char + str(self.assignType) + "\n"
         res += self.variable.printTree(indent + 1)
         res += self.expression.printTree(indent + 1) if issubclass(self.expression.__class__, classes.Node) else ((indent + 1) * indent_char) + str(self.expression) + '\n'
         return res
 
-    @addToClass(classes.MiddleAssignment)
+    @addToClass(classes.MiddleAssignment)  # works fine
     def printTree(self, indent=0):
-        res = indent * indent_char + self.assignType + "\n"
+        res = indent * indent_char + str(self.assignType) + "\n"
         res += self.variable.printTree(indent + 1)
         res += self.assignment.printTree(indent + 1)
         return res
@@ -78,44 +83,42 @@ class TreePrinter:
         res += self.print_expression.printTree(indent + 1)
         return res
 
-    @addToClass(classes.PrintExpression)
+    @addToClass(classes.PrintExpression)    #doesn't work
     def printTree(self, indent=0):
-        res = indent * indent_char + self.to_print + '\n'
+        res = indent * indent_char + str(self.to_print) + '\n'
         return res
 
-    @addToClass(classes.ReturnInstr)
+    @addToClass(classes.ReturnInstr)  # doesn't work because of printing expression, when expresion is number(int)
     def printTree(self, indent=0):
         res = indent * indent_char + "RETURN\n"
-        res += self.expression.printTree(indent + 1)
+        res += str(self.expression.printTree(indent + 1))
         return res
 
-    @addToClass(classes.Break)
+    @addToClass(classes.Break)  # works fine
     def printTree(self, indent=0):
         return indent * indent_char + " BREAK\n"
 
-    @addToClass(classes.Continue)
+    @addToClass(classes.Continue)  # works fine
     def printTree(self, indent=0):
         return indent * indent_char + " CONTINUE\n"
 
-
-    @addToClass(classes.ForInstruction)
+    @addToClass(classes.ForInstruction)     #work almost fine: range is not working because of expression
     def printTree(self, indent=0):
         res = ""
         res += indent * indent_char + "FOR" + "\n"
-        res += (indent + 1) * indent_char + self.var + "\n"
+        res += (indent + 1) * indent_char + str(self.var) + "\n"
         res += self.range.printTree(indent + 1)
-        res += self.instructionBlock.printTree(indent + 1)
+        res += self.instruction_block.printTree(indent + 1)
         return res
 
-
-    @addToClass(classes.While)
+    @addToClass(classes.While)      #works fine
     def printTree(self, indent=0):
         res = indent * indent_char + "WHILE\n"
         res += self.condition.printTree(indent + 1)
         res += self.instructions.printTree(indent + 1)
         return res
 
-    @addToClass(classes.If)
+    @addToClass(classes.If)         #works fine
     def printTree(self, indent=0):
         res = indent * indent_char + "IF\n"
         res += self.condition.printTree(indent + 1)
@@ -123,55 +126,50 @@ class TreePrinter:
         res += self.instructions.printTree(indent + 1)
         return res
 
-    @addToClass(classes.IfElse)
+    @addToClass(classes.IfElse)     #work fine
     def printTree(self, indent=0):
         res = indent * indent_char + "IF\n"
         res += self.condition.printTree(indent + 1)
         res += indent * indent_char + "THEN\n"
-        res += self.instrstructions.printTree(indent + 1)
+        res += self.instructions.printTree(indent + 1)
         res += indent * indent_char + "ELSE\n"
         res += self.else_instructions.printTree(indent + 1)
         return res
 
-    @addToClass(classes.Instructions)
+    @addToClass(classes.Instructions)  # works fine
     def printTree(self, indent=0):
         res = ""
         res += self.instructions.printTree(indent) if issubclass(self.instructions.__class__, classes.Node) else indent * indent_char + str(self.instructions) + '\n'
         res += self.instruction.printTree(indent)
         return res
 
-
     @addToClass(classes.Range)
     def printTree(self, indent=0):
         res = indent * indent_char + "RANGE\n"
-        res += (indent + 1) * indent_char + self.from_limit + "\n"
-        res += (indent + 1) * indent_char + self.to_limit + "\n"
+        res += (indent + 1) * indent_char + str(self.from_limit) + "\n"
+        res += (indent + 1) * indent_char + str(self.to_limit) + "\n"
         return res
 
-    @addToClass(classes.MatrixInitializer)
+    @addToClass(classes.MatrixInitializer)         # TODO
     def printTree(self, indent=0):
-        res = indent * indent_char + "MATRIX\n"             #TODO
+        res = indent * indent_char + "MATRIX\n"
         res += (indent + 1) * indent_char + "VECTOR\n"
         return res
 
-    @addToClass(classes.OnesInitFun)
+    @addToClass(classes.OnesInitFun)  # works fine
     def printTree(self, indent=0):
         res = indent * indent_char + "ones\n"
         res += (indent + 1) * indent_char + str(self.expression) + "\n"
         return res
 
-    @addToClass(classes.ZerosInitFun)
+    @addToClass(classes.ZerosInitFun)  # works fine
     def printTree(self, indent=0):
         res = indent * indent_char + "zeros\n"
         res += (indent + 1) * indent_char + str(self.expression) + "\n"
         return res
 
-    @addToClass(classes.EyeInitFun)
+    @addToClass(classes.EyeInitFun)  # works fine
     def printTree(self, indent=0):
         res = indent * indent_char + "eye\n"
         res += (indent + 1) * indent_char + str(self.expression) + "\n"
         return res
-
-
-
-
